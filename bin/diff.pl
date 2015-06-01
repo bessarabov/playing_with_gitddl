2f8b87182c2072f969d06432816b61c7e03fc73e
Ivan Bessarabov <ivan@bessarabov.ru>
2015-06-01 20:09:35 +0300

    bin/diff.pl

---
+++ bin/diff.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+
+=encoding UTF-8
+=cut
+
+=head1 DESCRIPTION
+
+=cut
+
+# common modules
+use strict;
+use warnings FATAL => 'all';
+use feature 'say';
+use utf8;
+use open qw(:std :utf8);
+
+use Carp;
+
+use lib::abs qw(
+    ../lib
+);
+use GitDDL;
+
+# global vars
+
+# subs
+
+# main
+sub main {
+
+    my $database = 'aaa';
+    my $host = 'db';
+    my $port = '3306';
+
+    my $gd = GitDDL->new(
+        work_tree => '/app', # git working directory
+        ddl_file  => 'sql/structure.sql',
+        dsn       => [
+            "DBI:mysql:database=$database;host=$host;port=$port",
+            'root',
+            'secret',
+        ],
+    );
+
+    use Data::Dumper;
+    warn Dumper $gd->diff;
+
+    say '#END';
+}
+main();
+__END__
